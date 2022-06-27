135c042c61d747b3924c68d9b45ac8542c50d67d
testgithubnayan <Nayan.soniPro@gmail.com>
2022-06-27 23:54:51 +0530

    employee wage and presence calculation

---
+++ empwage2.java
@@ -0,0 +1,23 @@
+package firstprogramme;
+
+public class empwage2 {
+
+
+	public static void main(String[] args) {
+		// calculating employee daily wage
+ int IS_FULL_TIME=1;
+ int RATE_PER_HOUR=20;
+ int empwage=0;
+ int emphour=0;
+ 
+	
+ double empcheck = Math.floor(Math.random() * 10) % 2;
+if (empcheck == IS_FULL_TIME);
+        emphour =8;
+  
+	  emphour =0;
+empwage = emphour * RATE_PER_HOUR;
+System.out.println("employee wage :-" + empwage);
+}
+
+}
