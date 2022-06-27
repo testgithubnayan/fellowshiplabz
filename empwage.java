d91f67604c74fcf6677f6d10d84b86709b0bc291
testgithubnayan <Nayan.soniPro@gmail.com>
2022-06-28 00:07:31 +0530

    employee wage problems//

---
+++ empwage.java
@@ -0,0 +1,14 @@
+
+public class empwage {
+
+	public static void main(String[] args) {
+		// employee present & absent code
+                  int IS_FULL_TIME = 1;
+                  double empckeck = Math.floor(Math.random() * 10) % 2;
+                  if (empcheck == IS_FULL_TIME)
+                		 System.out.println("present");
+                  else
+                	  System.out.println("absent")
+	}
+
+}
